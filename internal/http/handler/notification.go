839d02b1fc83019aaee448646c7929da04b504f1
javohir-a <abdusamatovjavohir@gmail.com>
2024-11-17 00:46:58 +0500

    working on handlers

--- internal/http/handler/notification.go
+++ internal/http/handler/notification.go
@@ -1,1 +1,9 @@
+/*
+ * @Author: javohir-a abdusamatovjavohir@gmail.com
+ * @Date: 2024-11-16 23:47:59
+ * @LastEditors: javohir-a abdusamatovjavohir@gmail.com
+ * @LastEditTime: 2024-11-17 00:46:25
+ * @FilePath: /tender/internal/http/handler/notification.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package handler
