4b9bc8a5dda9ec9064302f41cb086ddcf6697387
javohir-a <abdusamatovjavohir@gmail.com>
2024-11-16 23:42:39 +0500

    user updated

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,1 +1,18 @@
 package models
+
+type Role string
+
+var (
+	Client     Role = "Client"
+	Contractor Role = "Contractor"
+)
+
+type (
+	User struct {
+		ID       string `json:"id"`
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Role     Role   `json:"role"`
+	}
+)
